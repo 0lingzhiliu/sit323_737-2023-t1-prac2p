bd702e0cea7630c22998b7c4653946a0673419ed
0lingzhiliu <89500867+0lingzhiliu@users.noreply.github.com>
2023-03-19 22:16:36 +1100

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,10 @@
+var adder= function(first,second){
+    var sum=first+second
+    return sum
+}
+var log=function(msg){
+    console.log("[log]:",msg)
+}
+log("hello world")
+log("welcome to sit323/737")
+log("The sum is"+adder(5,6))
